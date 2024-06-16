abcac06f61687993eb4c98401f673c7fe1e83591
femiariel <149815504+femiariel@users.noreply.github.com>
2024-06-16 03:32:07 +0200

    Update str.py

--- spark/str.py
+++ spark/str.py
@@ -1,36 +1,27 @@
-
 from time import sleep
-
-from json import dumps
-from json import loads
-
-
-
-
-
+from json import dumps, loads
 import pyspark
 from pyspark.sql import functions as F
-from pyspark.sql.functions import explode
-#from pyspark.sql.functions import split
+from pyspark.sql.functions import explode, from_json, col
 from pyspark.sql.types import StringType, StructType, FloatType
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-#from pyspark.ml.feature import Tokenizer, RegexTokenizer
 import re
 from textblob import TextBlob
 
+# Initialiser une session Spark
 spark = SparkSession\
         .builder\
         .appName("RedditSentimentAnalysis")\
         .getOrCreate()
 
-
+# Définir le schéma des données entrantes
 schema = StructType() \
     .add("submission_id", StringType()) \
     .add("comment_id", StringType()) \
     .add("author", StringType()) \
     .add("body", StringType())
 
+# Lire les données de streaming à partir de Kafka
 df = spark \
     .readStream \
     .format("kafka") \
@@ -42,78 +33,76 @@ df = spark \
     .select(from_json(col("value"), schema).alias("data")) \
     .select("data.*")
 
+# Fonction pour nettoyer les commentaires
 def cleanTweet(tweet: str) -> str:
     tweet = re.sub(r'http\S+', '', str(tweet))
     tweet = re.sub(r'bit.ly/\S+', '', str(tweet))
     tweet = tweet.strip('[link]')
 
-    # remove users
+    # Remove users
     tweet = re.sub('(RT\s@[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
     tweet = re.sub('(@[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
 
-    # remove puntuation
+    # Remove punctuation
     my_punctuation = '!"$%&\'()*+,-./:;<=>?[\\]^_`{|}~•@â'
     tweet = re.sub('[' + my_punctuation + ']+', ' ', str(tweet))
 
-    # remove number
+    # Remove numbers
     tweet = re.sub('([0-9]+)', '', str(tweet))
 
-    # remove hashtag
+    # Remove hashtags
     tweet = re.sub('(#[A-Za-z]+[A-Za-z0-9-_]+)', '', str(tweet))
 
-    tweet = re.sub(r'@[A-Za-z0–9]+', '', str(tweet)) #Remove @mentions replace with blank
-    tweet = re.sub(r'#', '', str(tweet)) #Remove the ‘#’ symbol, replace with blank
-    tweet = re.sub(r'RT[\s]+', '', str(tweet)) #Removing RT, replace with blank
-    tweet = re.sub(r'https?:\/\/\S+', '', str(tweet)) #Remove the hyperlinks
-    tweet = re.sub(r':', '', str(tweet)) # Remove :
+    tweet = re.sub(r'@[A-Za-z0–9]+', '', str(tweet))  # Remove @mentions
+    tweet = re.sub(r'#', '', str(tweet))  # Remove the ‘#’ symbol
+    tweet = re.sub(r'RT[\s]+', '', str(tweet))  # Removing RT
+    tweet = re.sub(r'https?:\/\/\S+', '', str(tweet))  # Remove hyperlinks
+    tweet = re.sub(r':', '', str(tweet))  # Remove :
     tweet = re.sub(r'\\','',str(tweet))
     tweet = re.sub(r'\>','',str(tweet))
     tweet = re.sub(r'"','',str(tweet))
     tweet = re.sub(r'“','',str(tweet))
-    
 
     return tweet
 
-
-#Next we have to remove emoji & Unicode from the Tweet data.
+# Fonction pour supprimer les emojis et Unicode
 def remove_emoji(string):
- emoji_pattern = re.compile('['
- u"\U0001F600-\U0001F64F" # emoticons
- u"\U0001F300-\U0001F5FF" # symbols & pictographs
- u"\U0001F680-\U0001F6FF" # transport & map symbols
- u"\U0001F1E0-\U0001F1FF" # flags (iOS)
- u"\U00002500-\U00002BEF" # chinese char
- u"\U00002702-\U000027B0"
- u"\U00002702-\U000027B0"
- u"\U000024C2-\U0001F251"
- u"\U0001f926-\U0001f937"
- u"\U00010000-\U0010ffff"
- u"\u2640-\u2642"
- u"\u2600-\u2B55"
- u"\u200d"
- u"\u23cf"
- u"\u23e9"
- u"\u231a"
- u"\ufe0f" # dingbats
- u"\u3030"
- "]+", flags=re.UNICODE)
- return emoji_pattern.sub(r'', string)
+    emoji_pattern = re.compile('['
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642"
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+        "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
 
+# Définir les UDFs pour nettoyer les tweets
 clean_tweets = F.udf(cleanTweet, StringType())
 raw = df.withColumn('pre_processed_text', clean_tweets(col("body")))
 comments = raw.withColumn('processed_text', clean_tweets(col("pre_processed_text")))
 
-
-# Create a function to get the subjectifvity
+# Fonction pour obtenir la subjectivité
 def getSubjectivity(tweet: str) -> float:
     return TextBlob(tweet).sentiment.subjectivity
 
-
-# Create a function to get the polarity
+# Fonction pour obtenir la polarité
 def getPolarity(tweet: str) -> float:
     return TextBlob(tweet).sentiment.polarity
 
-
+# Fonction pour obtenir le sentiment à partir de la polarité
 def getSentiment(polarityValue: int) -> str:
     if polarityValue < 0:
         return 'Negative'
@@ -121,12 +110,13 @@ def getSentiment(polarityValue: int) -> str:
         return 'Neutral'
     else:
         return 'Positive'
-    
 
+# Définir les UDFs pour la subjectivité, polarité et sentiment
 subjectivity = F.udf(getSubjectivity, FloatType())
 polarity = F.udf(getPolarity, FloatType())
 sentiment = F.udf(getSentiment, StringType())
 
+# Appliquer les transformations pour obtenir la subjectivité, polarité et sentiment
 subjectivity_tweets = comments.withColumn('subjectivity', subjectivity(col("processed_text")))
 polarity_tweets = subjectivity_tweets.withColumn("polarity", polarity(col("processed_text")))
 sentiment_tweets = polarity_tweets.withColumn("sentiment", sentiment(col("polarity")))
@@ -142,7 +132,6 @@ query = sentiment_tweets \
 
 # Rester en attente pour que le streaming continue
 query.awaitTermination()
-
 
 """
 # Interroger et afficher les résultats périodiquement
